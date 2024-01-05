6f8fe31207b0f6b367c7c316cd81a63178902a07
LanaLazarevic <llazarevic6021rn@raf.rs>
2024-01-05 11:57:35 +0100

    Klijent

--- UserService/src/main/java/com/projekat2/UserService/domain/Client.java
+++ UserService/src/main/java/com/projekat2/UserService/domain/Client.java
@@ -2,6 +2,7 @@ package com.projekat2.UserService.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import org.apache.commons.lang3.RandomStringUtils;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,11 +13,20 @@ import lombok.Setter;
 public class Client extends User {
     private Integer membershipNumber;
     private Integer sessionCount;
+    private String activateCode;
+    private boolean activate;
+    private boolean blocked;
 
     public Client(String email, String firstName, String lastName, String username, String password, Integer membershipNumber, Integer sessionCount) {
         super(email, firstName, lastName, username, password);
         this.membershipNumber = membershipNumber;
         this.sessionCount = sessionCount;
+
+        int desiredLength = 10;
+        this.activateCode = RandomStringUtils.randomAlphanumeric(desiredLength);
+        this.activate = true;
+        this.blocked = false;
+
     }
 
     public Client() {
