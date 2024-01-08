40fd2dfaed151794cb1d221232c3cef6b7b2deb6
vassilijee <45370993+vassilijee@users.noreply.github.com>
2024-01-08 18:49:46 +0100

    Delete UserService/src/main/java/com/projekat2/UserService/repository/AdminRepository.java

--- UserService/src/main/java/com/projekat2/UserService/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projekat2.UserService.repository;
-
-import com.projekat2.UserService.domain.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
